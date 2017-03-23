723f2a8caaa8e588d8d2e2d68715068a39dcda6e
EgorOmelyanenko <egaom@mail.ru>
2017-03-23 14:55:50 +0700

    test

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import asyncio
 async def fetch(client):
     url = 'http://localhost:8080/'
     data={}
-    data["text"] = "abc"
+    data["text"] = "abcde"
     async with client.post(url,data=data) as resp:
           return await resp.text()
 
